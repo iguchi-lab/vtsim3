20ec4aa283260b6a68f614bae7bd4f807b91953f
IGUCHI Masato <67450288+iguchi-lab@users.noreply.github.com>
2022-01-08 23:55:21 +0900

    Update node.h

--- node.h
+++ node.h
@@ -20,13 +20,4 @@ public:
         if(get<2>(flag) == SN_CALC)   h_inp.assign(length, 0.0);
     }
 
-    void set_P(vector<double> p_)           {p     = p_;}    //圧力固定値
-    void set_C(vector<double> c_)           {c     = c_;}    //濃度固定値
-    void set_T(vector<double> t_)           {t     = t_;}    //温度固定値
-    void set_h_sr(vector<double> h_sr_)     {h_sr  = h_sr_;}
-    void set_h_inp(vector<double> h_inp_)   {h_inp = h_inp_;}
-    void set_v(vector<double> v_)           {v     = v_;}
-    void set_capa(int s_i_)                 {s_i = s_i_;}
-    void set_m(vector<double> m_)           {m     = m_;}
-    void set_beta(vector<double> beta_)     {beta  = beta_;}
 };
