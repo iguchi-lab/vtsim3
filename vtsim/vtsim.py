333c3254af71ee94dde2e5a2cf31ca8d5b6f0412
IGUCHI Masato <67450288+iguchi-lab@users.noreply.github.com>
2022-01-09 14:26:06 +0900

    Update vtsim.py

--- vtsim/vtsim.py
+++ vtsim/vtsim.py
@@ -87,7 +87,7 @@ def run_calc(ix, sn, **kwargs):                                                 
 
     print('Start vtsim calc.')
     s_time = time.time()
-    calc.calc()
+    #calc.calc()
     e_time = time.time() - s_time    
     print('Finish vtsim calc.')
     print("calc time = {0}".format(e_time * 1000) + "[ms]")
@@ -193,8 +193,8 @@ def set_thrm_net(sn, **kwargs):
         print(i, len(sn) + i, len(tn) + i)
         node[d_node(n['name'])] = len(sn) + i                                               #時間遅れノードのノード番号
         
-        calc.sn_add(len(sn) + i, [SN_NONE, SN_NONE, SN_DLY])                                #計算フラグ
-        if 't' in n:    calc.sn[len(sn) + i].t = to_list_f(n['t'])
+        #calc.sn_add(len(sn) + i, [SN_NONE, SN_NONE, SN_DLY])                                #計算フラグ
+        #if 't' in n:    calc.sn[len(sn) + i].t = to_list_f(n['t'])
 
         #calc.tn_add(len(tn) + i, node[d_node(n['name'])], node[n['name']], TN_SIMPLE)       #熱容量の設定
         #calc.tn[len(tn) + i].cdtc = to_list_f(n['capa'] / sts.t_step)                       #コンダクタンス（熱容量）            
